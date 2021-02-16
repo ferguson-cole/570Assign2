12a70533a1099ba4a41eee6e17a6566cd8d49809
Cole <coleferguson9@gmail.com>
2021-02-15 19:28:48 -0800

    added main fnction

--- progbar.cpp
+++ progbar.cpp
@@ -1,3 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    if(argc <= 0) printf("No file specified.");
+}
+
+
+
 void * progress_monitor(void *) {
     typedef struct {
         long *CurrentStatus;
