0010020780b4a646f3301b6017e5d8992c58897d
Cole <coleferguson9@gmail.com>
2021-02-15 19:47:26 -0800

    additional content to main

--- progbar.cpp
+++ progbar.cpp
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include <stdio>
+#include <cstring>
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    // As of right now, only setting up for one argument
     if(argc <= 0) printf("No file specified.");
-}
+    else if (argc >= 2) printf("Too many arguments provided.");
 
+    // Call the wordcount function
+    long count = wordcount();
+    if(count == nullptr) printf("An error has occurred. Word count unknown.");
+    string filename = "placeholder.txt";
+    else printf("There are %ld words in %s.", count, filename);
+}
 
 
 void * progress_monitor(void *) {
@@ -13,4 +23,9 @@ void * progress_monitor(void *) {
         long InitialValue;
         long TerminationValue;
     } PROGRESS_STATUS;
+    return;
+}
+
+long wordcount(char *fd) {
+    return 55;
 }
