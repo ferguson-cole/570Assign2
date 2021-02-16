e80f5c638c49c232802803b54b285f9b4b156503
Cole <coleferguson9@gmail.com>
2021-02-15 20:42:54 -0800

    added file permission err handling to main()

--- progbar.cpp
+++ progbar.cpp
@@ -1,22 +1,30 @@
+/*
+I the undersigned promise that the attached assignment is my own work. 
+While I was free to discuss ideas with others, the work contained is my own.
+I recognize that should this not be the case, I will be subject to penalties 
+as outlined in the course syllabus.
+Cole Ferguson, RedID - 820562542
+*/
 #include <stdio>
 #include <cstring>
 #include <ifstream>
+#define FILE_ERR -2
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     // As of right now, only setting up for one argument
-    if(argc <= 0) printf("No file specified.");
-    else if (argc >= 2) printf("Too many arguments provided.");
+    if(argc <= 0) cout << "No file specified.";
+    else if (argc >= 2) cout << "Too many arguments provided.";
 
     // Call the wordcount function
     long count = wordcount(argv[0]);
 
-    if(count > -1) printf("There are %ld words in %s.", count, filename);
-    else printf("An error has occurred. Word count unknown.");
+    if(count > -1) cout << "There are " << count << " words in " << argv[0] << ".";
+    else if (count == FILE_ERR) cout << "Could not open file.";
+    else cout << "An error has occurred. Word count unknown.";
 }
-
 
 void * progress_monitor(void *) {
     typedef struct {
@@ -28,5 +36,14 @@ void * progress_monitor(void *) {
 }
 
 long wordcount(char *fd) {
+    try
+    {
+        fstream;
+    }
+    catch(const exception& e)
+    {
+        return FILE_ERR;
+    }
+    
     return -1;
 }
